c6166429b90bb4e862d93862b6ef45a810f12c2f
Ecaterina Baba <e.baba@innopolis.ru>
2018-10-07 18:42:57 +0300

    Connect to postgresql filelds
    Waiting for db info

--- acme_backend/settings.py
+++ acme_backend/settings.py
@@ -75,9 +75,14 @@ WSGI_APPLICATION = 'acme_backend.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
+    #TODO
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db_name',
+        'USER': 'db_user',
+        'PASSWORD': 'db_user_password',
+        'HOST': '',
+        'PORT': 'db_port_number',
     }
 }
 
