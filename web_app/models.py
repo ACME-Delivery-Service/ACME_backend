e3fc55b10118f806b36a1c4fcc2e00ebcee26037
DanMagor <anybis-maat@mail.ru>
2018-10-29 21:40:33 +0300

    BACKEND-31: DB models reworked

--- web_app/models.py
+++ web_app/models.py
@@ -1,49 +1,82 @@
 from django.db import models
+from django.contrib.gis.geos import Point  # TODO: CHECK FOR POINT TYPE
+from django.contrib.postgres.fields import ArrayField
 
-
-class User(models.Model):
-    login = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
-    email = models.EmailField()
-
-
-class Parcel(models.Model):
-    weight = models.FloatField()
-    volume = models.FloatField()
-    # TODO: define ENUM of standard shapes
-    shape = models.CharField(max_length=100)
-
-
-class IncomingOrder(models.Model):
-    created_on = models.TimeField()
-    priority = models.IntegerField()
-    parcel_id = models.ForeignKey(Parcel, on_delete=models.CASCADE)
-
-    @property
-    def incoming_orders_order_id_fkey(self):
-        return self.parcel_id.id
-
-
-class DispatchOrder(models.Model):
-    incoming_order_id = models.ForeignKey(IncomingOrder, on_delete=models.CASCADE)
-
-    @property
-    def dispatch_order_incoming_order_id_fkey(self):
-        return self.incoming_order_id.id
+from enum import Enum
 
 
 class Contact(models.Model):
+    contact_id = models.CharField()
     address = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=20)
-    additional_info = models.CharField(max_length=255)
+    additional_info = models.TextField()
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     position = models.CharField(max_length=255)
     company = models.CharField(max_length=255)
 
 
+class AcmeCustomer(models.Model):
+    contact_id = models.ForeignKey(Contact, on_delete=models.DO_NOTHING)
+
+    @property
+    def customer_contact_id(self):
+        return self.contact_id.id
+
+
+class Location(models.Model):
+    location_address = models.CharField(max_length=255)
+    lat_long = Point()  # TODO: Check that it's right
+
+
+class DeliveryPeriod(models.Field):
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+
+
+class AcmeOrder(models.Model):
+    created_on = models.DateTimeField()
+    comment = models.TextField()
+    customer_id = models.ForeignKey(AcmeCustomer, on_delete=models.PROTECT)
+    priority = models.IntegerField()
+    start_location_id = models.ForeignKey(Location, on_delete=models.PROTECT)
+    end_location_id = models.ForeignKey(Location, on_delete=models.PROTECT)
+    scheduled_time = DeliveryPeriod()
+
+    @property
+    def acme_order_start_location_id(self):
+        return self.start_location_id.id
+
+    @property
+    def acme_order_end_location_id(self):
+        return self.end_location_id.id
+
+    @property
+    def customer_id_fkey(self):
+        return self.customer_id.id
+
+
+class ShapeTypes(Enum):
+    POSTCARD = 'postcard'
+    LETTER = 'letter'
+    LARGE_ENVELOPE = 'large_envelope'
+    PARCEL = 'parcel'
+
+
+class Parcel(models.Model):
+    weight = models.FloatField()
+    dimension = ArrayField(models.FloatField(), size=3)
+    shape = models.CharField(choices=[(tag, tag.value) for tag in ShapeTypes])
+    order_id = models.ForeignKey(AcmeOrder, on_delete=models.CASCADE)
+
+    @property
+    def order_id_fkey(self):
+        return self.order_id.id
+
+
 class Warehouse(models.Model):
-    contact_id = models.ForeignKey(Contact, on_delete=models.CASCADE)
+    warehouse_name = models.CharField(max_length=255)
+    contact_id = models.ForeignKey(Contact, on_delete=models.DO_NOTHING)
     max_capacity = models.FloatField()
     is_active = models.BooleanField(default=True)
 
@@ -52,50 +85,112 @@ class Warehouse(models.Model):
         return self.contact_id.id
 
 
-class DispatchStatus(models.Model):
-    created_on = models.TimeField()
-    status = models.CharField(max_length=20)
-    # TODO: make ENUM of all possible statuses.
-    warehouse_id = models.ForeignKey(Warehouse, on_delete=models.CASCADE)  # OPTIONAL
-    dispatch_order_id = models.ForeignKey(DispatchOrder, on_delete=models.CASCADE)
+class OrderStatusType(Enum):
+    CREATED = 'created'
+    APPROVED = 'approved'
+    EN_ROUT = 'en_rout'
+    STORED = 'stored'
+    DELIVERED = 'delivered'
+
+
+class AcmeOrderStatus(models.Model):
+    created_on = models.DateTimeField()
+    status = models.CharField(choices=[(tag, tag.value) for tag in OrderStatusType])
+    warehouse_id = models.ForeignKey(Warehouse, on_delete=models.DO_NOTHING)
+    order_id = models.ForeignKey(AcmeOrder, on_delete=models.PROTECT)
+
+    class Meta:
+        unique_together = (('order_id', 'created_on'),)
 
     @property
-    def dispatch_status_warehouse_id_fkey(self):
+    def orders_warehouse_id(self):
         return self.warehouse_id.id
 
     @property
-    def dispatch_status_pkey(self):
-        return self.dispatch_order_id.id
-    # TODO check if it is correct
+    def orders_order_id(self):
+        return self.order_id.id
 
 
-# TODO: Add 'next_vector' parameter.
-class TransportationVector(models.Model):
-    weather = models.CharField(max_length=50)
-    traffic = models.CharField(max_length=255)
+class AcmeLocations(Enum):
+    EU = 'EU'
+    RU = 'RU'
+    CH = 'CH'
+    UK = 'UK'
 
 
-# TODO: Fix relation between routes, vectors and operators.
-class TransportationRoutes(models.Model):
-    transport_type = models.CharField(max_length=50)
-    start_location = models.CharField(max_length=255)
-    end_location = models.CharField(max_length=255)
+class AcmeRoles(Enum):
+    CEO = 'CEO'
+    DO = 'DO'
+    CO = 'CO'
+    CS = 'CS'
+    CD = 'CD'
+
+
+class AcmeUser(models.Model):
+    password = models.CharField()
+    user_location = models.CharField(max_legth=5, choices=[(tag, tag.value) for tag in AcmeLocations])
+    email = models.EmailField(max_length=255, unique=True)
+    contact_id = models.ForeignKey(Contact, on_delete=models.DO_NOTHING)
+    token = models.CharField(max_length=255, unique=True)
+    file_url = models.CharField(max_length=255)
+
+    @property
+    def users_contact_id(self):
+        return self.contact_id.id
+
+
+class UserRole(models.Model):
+    user_id = models.ForeignKey(AcmeUser)
+    role = models.CharField(choices=[(tag, tag.value) for tag in AcmeRoles])
+
+    @property
+    def roles_users_id(self):
+        return self.user_id.id
+
+
+class DeliveryStatusTypes(Enum):
+    PENDING = 'pending'
+    IN_PROGRESS = 'in_progress'
+    COMPLETED = 'completed'
 
 
 class DeliveryOperator(models.Model):
-    contact_id = models.ForeignKey(Contact, on_delete=models.CASCADE)
+    operator_id = models.ForeignKey(AcmeUser, on_delete=models.PROTECT)
+    current_pos = models.ForeignKey(Location, on_delete=models.DO_NOTHING)
+    pos_last_updated = models.DateTimeField()
 
     @property
-    def delivery_operators_contact_id(self):
-        return self.contact_id.id
-
-
-class TransportationCompany(models.Model):
-    contact_id = models.ForeignKey(Contact, on_delete=models.CASCADE)
-    transportation_types = models.CharField(max_length=50)
-
-    # TODO: Should we create separate table or enum for types?
+    def users_id(self):
+        return self.operator_id.id
 
     @property
-    def transportation_companies_contact_id_fkey(self):
-        return self.contact_id.id
+    def users_location_id(self):
+        return self.current_pos.id
+
+
+class OrderDelivery(models.Model):
+    order_id = models.ForeignKey(AcmeUser, on_delete=models.CASCADE)
+    delivery_operator_id = models.ForeignKey(DeliveryOperator, on_delete=models.CASCADE)
+    delivery_status = models.CharField(choices=[(tag, tag.value) for tag in DeliveryStatusTypes])
+    start_location_id = models.ForeignKey(Location, on_delete=models.PROTECT)
+    end_location_id = models.ForeignKey(Location, on_delete=models.PROTECT)
+    active_time_period = ArrayField(DeliveryPeriod)
+
+    class Meta:
+        unique_together = (('order_id', 'delivery_operator_id'))
+
+    @property
+    def orders_id_fkey(self):
+        return self.order_id.id
+
+    @property
+    def orders_delivery_operator_id(self):
+        return self.delivery_operator_id.id
+
+    @property
+    def acme_order_start_location_id(self):
+        return self.start_location_id.id
+
+    @property
+    def acme_order_end_location_id(self):
+        return self.end_location_id.id
