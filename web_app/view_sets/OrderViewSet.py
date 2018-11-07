c07ee38ca482131714d386d8119fcfd1a2ec1449
Egor Ivkov <e.o.ivkov@gmail.com>
2018-11-07 21:48:23 +0300

    Order info untested

--- web_app/view_sets/OrderViewSet.py
+++ web_app/view_sets/OrderViewSet.py
@@ -4,6 +4,9 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
 
+from web_app.models import AcmeOrder, Location, Parcel, AcmeCustomer, Contact, AcmeOrderStatus
+import json
+
 
 class OrderViewSet(viewsets.ViewSet):
 
@@ -25,54 +28,67 @@ class OrderViewSet(viewsets.ViewSet):
 
     @action(detail=True, methods=['GET'], permission_classes=[IsAuthenticated])
     def info(self, request, pk=None):
-        if not pk:
-            return Response({'msg': 'No ID provided'}, HTTP_400_BAD_REQUEST)
-
-        return Response({
-            'id': pk,
-            'delivery_period': {
-                'start': '2018-12-25 12:20:00',
-                'end': '2018-01-25 10:10:00'
-            },
-            'priority': 242,
-            'address_from': {
-                'address': 'Infinite loop, 1, Cupertino, CA, USA',
-                'location': {
-                    'latitude': 12343526.31,
-                    'longitude': 42445698.7
-                }
-            },
-            'address_to': {
-                'address': 'Infinite loop, 1, Cupertino, CA, USA',
-                'location': {
-                    'latitude': 35664564.31,
-                    'longitude': 67367546.3
-                }
-            },
-            'description': 'The client asked not to knock on the door, just leave it.',
-            'parcels_info': [
-                {
-                    'id': 123312,
-                    'weight': 1.38,
-                    'dimensions': {
-                        'x': 123.0,
-                        'y': 123.0,
-                        'z': 123.0
-                    },
-                    'shape': 'Unsupported yet',
-                    'description': 'Please be cautious while moving this one.'
-                }
-            ],
-            'customer_info': {
-                'id': 1243,
-                'contacts': {
-                    'first_name': 'Johnathan',
-                    'last_name': 'Morrison',
-                    'phone_number': '+1123412341234'
+        try:
+            order = AcmeOrder.objects.get(pk=pk)
+            start_location = Location.objects.get(pk=order.start_location_id)
+            end_location = Location.objects.get(pk=order.end_location_id)
+            parcels = Parcel.objects.filter(order_id=pk)
+            customer = AcmeCustomer.objects.get(pk=order.customer_id)
+            customer_contact = Contact.objects.get(pk=customer.pk)
+            order_statuses = AcmeOrderStatus.objects.filter(order_id=order.pk)
+            current_status = order_statuses.order_by('created_on').last()
+            status = "None"
+            if current_status != None:
+                status = current_status.status
+            parcels_info = []
+            for parcel in parcels:
+                parcel_info = {
+                        'id': parcel.pk,
+                        'weight': parcel.weight,
+                        'dimensions': {
+                            'x': parcel.dimension[0],
+                            'y': parcel.dimension[1],
+                            'z': parcel.dimension[2],
+                        },
+                        'shape': parcel.shape,
+                        'description': "No description provided."
+                    }
+                parcels_info.append(parcel_info)
+            return Response({
+                'id': pk,
+                'delivery_period': {
+                    'start': order.scheduled_time.start_time,
+                    'end': order.scheduled_time.end_time
                 },
-            },
-            'status': 'pending'
-        }, status=HTTP_200_OK)
+                'priority': order.priority,
+                'address_from': {
+                    'address': start_location.location_address,
+                    'location': {
+                        'latitude': start_location.lat_long.x,
+                        'longitude': start_location.lat_long.y
+                    }
+                },
+                'address_to': {
+                    'address': end_location.location_address,
+                    'location': {
+                        'latitude': end_location.lat_long.x,
+                        'longitude': end_location.lat_long.y
+                    }
+                },
+                'description': order.comment,
+                'parcels_info': parcels_info,
+                'customer_info': {
+                    'id': customer.pk,
+                    'contacts': {
+                        'first_name': customer_contact.first_name,
+                        'last_name': customer_contact.last_name,
+                        'phone_number': customer_contact.phone_number
+                    },
+                },
+                'status': status
+            }, status=HTTP_200_OK)
+        except AcmeOrder.DoesNotExist:
+            return Response({'msg': 'No ID provided'}, HTTP_400_BAD_REQUEST)
 
     @action(detail=False, methods=['GET'], permission_classes=[IsAuthenticated])
     def location(self, request):
