c9cc9c63587ed180d73a6c29ca732c718c2158a7
Egor Ivkov <e.o.ivkov@gmail.com>
2018-11-12 15:41:17 +0300

    order info compatibility corrections

--- web_app/serializers.py
+++ web_app/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+
 from .models import *
 
 
@@ -9,7 +10,10 @@ class ContactSerializer(serializers.ModelSerializer):
 
 
 class AcmeCustomerSerializer(serializers.ModelSerializer):
-    contact = ContactSerializer()
+    contacts = serializers.SerializerMethodField()
+
+    def get_contacts(self, obj):
+        return ContactSerializer(obj.contact).data
 
     class Meta:
         model = AcmeCustomer
@@ -21,18 +25,74 @@ class AcmeOrderStatusSerializer(serializers.ModelSerializer):
         model = AcmeOrderStatus
         fields = '__all__'
 
+
 class LocationSerializer(serializers.ModelSerializer):
+    location = serializers.SerializerMethodField()
+
+    def get_location(self, loc):
+        return {
+            'latitude': loc.latitude,
+            'longitude': loc.longitude,
+        }
     class Meta:
         model = Location
-        fields = '__all__'
+        fields = ('address', 'location')
+
 
 class AcmeOrderSerializer(serializers.ModelSerializer):
-    customer = AcmeCustomerSerializer()
-    start_location = LocationSerializer()
-    end_location = LocationSerializer()
+    customer_info = serializers.SerializerMethodField()
+    address_from = serializers.SerializerMethodField()
+    address_to = serializers.SerializerMethodField()
+    description = serializers.SerializerMethodField()
+    parcels_info = serializers.SerializerMethodField()
+    status = serializers.SerializerMethodField()
+    delivery_period = serializers.SerializerMethodField()
+
+    def get_customer_info(self, obj):
+        return AcmeCustomerSerializer(obj.customer).data
+
+    def get_address_from(self, obj):
+        return LocationSerializer(obj.start_location).data
+
+    def get_address_to(self, obj):
+        return LocationSerializer(obj.end_location).data
+
+    def get_delivery_period(self, obj):
+        return {
+            'start': obj.scheduled_time_start_time,
+            'end': obj.scheduled_time_end_time
+        }
+
+    def get_status(self, obj):
+        order_statuses = AcmeOrderStatus.objects.filter(order_id=obj.pk)
+        current_status = order_statuses.order_by('created_on').last()
+        return current_status.status if current_status != None else "None"
+
+    def get_parcels_info(selfself, obj):
+        parcels = Parcel.objects.filter(order_id=obj.pk)
+        parcels_info = []
+        for parcel in parcels:
+            parcel_info = {
+                'id': parcel.pk,
+                'weight': parcel.weight,
+                'dimensions': {
+                    'x': parcel.dimension[0],
+                    'y': parcel.dimension[1],
+                    'z': parcel.dimension[2],
+                },
+                'shape': parcel.shape,
+                'description': "No description provided."
+            }
+            parcels_info.append(parcel_info)
+        return parcels_info
+
+    def get_description(self, obj):
+        return obj.comment
+
     class Meta:
         model = AcmeOrder
-        fields = '__all__'
+        fields = ('id', 'delivery_period', 'priority', 'address_from', 'address_to', 'description', 'parcels_info',
+                  'customer_info', 'status')
 
 
 class AcmeUserSerializer(serializers.ModelSerializer):
@@ -63,6 +123,7 @@ class AcmeOrderDeliverySerializer(serializers.ModelSerializer):
     delivery_operator = AcmeDeliveryOperatorSerializer()
     start_location = LocationSerializer()
     end_location = LocationSerializer()
+
     class Meta:
         model = OrderDelivery
         fields = '__all__'
