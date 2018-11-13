2d286a4fb1aa647ae3ede8df23857526bfc819ca
Sergey <sergejrf@gmail.com>
2018-11-13 16:39:24 +0300

    Added customer mock

--- db/mock_data.sql
+++ db/mock_data.sql
@@ -6,4 +6,7 @@ ALTER TABLE web_app_userrole DISABLE TRIGGER ALL;
 INSERT INTO web_app_userrole VALUES (1, 'CEO', 1) ON CONFLICT DO NOTHING;
 ALTER TABLE web_app_userrole ENABLE TRIGGER ALL;
 
+INSERT INTO web_app_contact VALUES (2, 'Innopolis, Sportivnaya 10', '+7(900) 666-35-35', '', 'Egor', 'Baba', '', '') ON CONFLICT DO NOTHING;
+INSERT INTO web_app_acmecustomer VALUES(1, 2);
+
 COMMIT;
